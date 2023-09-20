a7c1d488676d5f36fbe5749c3fe6f3d83f367f94
Cristina Rivera <cristinapruebas14@gmail.com>
2023-09-20 19:30:17 +0200

    php intro variables

---
+++ web/hola.php
@@ -0,0 +1,16 @@
+<?php
+echo "Hola\n";
+echo cos(4 * 3);
+echo "\n";
+// comentario 1
+# comentario 2
+/* comentario 3 */
+/**
+ * comentario de documentación
+ */
+$x = 5;
+$y = 4;
+echo $x + $y;
+echo "\n";
+unset($y);
+?>
