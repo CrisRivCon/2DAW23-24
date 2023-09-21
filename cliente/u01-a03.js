7b8aa5ead919625429171abb3287658b2cc01f7c
Cristina Rivera <cristinapruebas14@gmail.com>
2023-09-21 21:14:00 +0200

    formulario

--- cliente/u01-a03.js
+++ cliente/u01-a03.js
@@ -1,7 +1,24 @@
-function consoleMsg() {
-    console.log("Mensaje por consola.");
-  }
+function ing(){
+  document.write(enText);
+}
+function ru(){
+  document.write(ruText);
+}
+function es(){
+  document.write(esText);
+}
 
-var button = document.getElementById("a");
-button.addEventListener("click", consoleMsg);
+var button1 = document.getElementById("a");
+var button2 = document.getElementById("b");
+var button3 = document.getElementById("c");
+
+var enText = "<p style=\"color: orange;\">Welcome!</p>";
+var ruText = "<p style=\"color: blue;\">добро пожаловать</p>";
+var esText = "<p style=\"color: green;\">Bienvenida!</p>";
+
+button1.addEventListener("click", ing);
+
+button2.addEventListener("click", ru);
+
+button3.addEventListener("click", es);
 
--- web/hola.php
+++ web/hola.php
@@ -13,4 +13,17 @@ $y = 4;
 echo $x + $y;
 echo "\n";
 unset($y);
+
+// asignación por referencia, los dos comparten la variable.
+$x =& $y;
+
+// expansión de variable o interpolación de cadenas, sucede con las comillas dobles.
+// siempre que sea posible utilizar comillas simples.
+
+// concatenar con .
+// no usar $x[1]  con cadenas, usar funciones predefinidas .
+
+
+
+
 ?>
