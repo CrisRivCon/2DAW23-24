e87ea2e4def32913ac4a390a06f9936b2fb19d6d
Cristina Rivera <cristinapruebas14@gmail.com>
2023-10-02 20:36:07 +0200

    u02 A14,15

---
+++ cliente/U02-A14-Cristina/U02-A14-Cristina.js
@@ -0,0 +1,38 @@
+var boton = document.getElementById("boton");
+
+function mostrarPantalla(res, texto){
+    while(res.hasChildNodes()){
+        res.removeChild(res.firstChild);
+    }
+    let p = document.createElement("p");
+    p.innerHTML = texto;
+    res.append(p);
+}
+
+function traductor(){
+    let palabra = prompt("Introduce la palabra a traducir:")
+    var res = document.getElementById("resultado");
+    switch (palabra) {
+        case "casa":
+            mostrarPantalla(res, "house")
+            break;
+
+        case "mesa":
+            mostrarPantalla(res, "table")
+            break;
+
+        case "perro":
+            mostrarPantalla(res, "dog")
+            break;
+
+        case "gato":
+            mostrarPantalla(res, "gato")
+            break;
+
+        default:
+            mostrarPantalla(res, "Palabra no válida.")
+            break;
+    }
+}
+
+boton.addEventListener("click", traductor);
---
+++ cliente/U02-A15-Cristina/U02-A15-Cristina.js
@@ -0,0 +1,15 @@
+function descomponerFrase(){
+    let frase = prompt("Introduce la frase a descomponer.")
+    let res = document.getElementById("resultado");
+    while(res.hasChildNodes()){
+        res.removeChild(res.firstChild);
+    }
+    for (let i of frase){
+        if (isNaN(i) || i == " "){
+            let p = document.createElement("p");
+            p.innerHTML = i;
+            res.append(p);
+        }
+    }
+}
+document.getElementById("boton").addEventListener("click", descomponerFrase);
