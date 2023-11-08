718bf72c0bd58f179d1b6a183ace6511f2ffcacc
Cristina Rivera <cristinapruebas14@gmail.com>
2023-11-08 21:16:02 +0100

    Comienzo T4!

---
+++ cliente/u04/U04-A01-Cristina/U04-A01-Cristina.js
@@ -0,0 +1,12 @@
+var arrayAsociativo = new Array();
+arrayAsociativo.par1 = 'par1';
+arrayAsociativo.par2 = 'par2';
+arrayAsociativo.par3 = 'par3';
+
+function ordenarArray(arrayAsociativo) {
+    var ordenado = new Array();
+    var objectOrdenado = [];
+    
+}
+
+ordenarPorIndice(arrayAsociativo);
