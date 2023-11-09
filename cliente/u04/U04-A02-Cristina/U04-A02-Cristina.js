9149585f530a3acf840036132df9d4fb81fea608
Cristina Rivera <cristinapruebas14@gmail.com>
2023-11-09 17:28:27 +0100

    Cliente

---
+++ cliente/u04/U04-A02-Cristina/U04-A02-Cristina.js
@@ -0,0 +1,31 @@
+var respuesta = document.getElementById('respuesta');
+var vector = [];
+
+function vectorRandom(vector) {
+    for (let i = 0; i < 10 ; i++) {
+        vector.push(Math.trunc(Math.random() * 1000));
+    }
+    escribir(vector.join(', '));
+}
+
+function nuevoVector(vector) {
+    let nuevoVector = vector.slice();
+    nuevoVector.sort(function (a, b) { return a - b });
+    nuevoVector.pop();
+    nuevoVector.shift();
+    escribir(nuevoVector.join(', '));
+}
+
+function escribir(res) {
+    let p = document.createElement('p');
+    p.textContent = res;
+    respuesta.append(p);
+}
+
+
+vectorRandom(vector);
+let max = Math.max(...vector);
+let min = Math.min(...vector);
+escribir(`El máximo es ${max}`);
+escribir(`El mínimo es ${min}`);
+nuevoVector(vector);
---
+++ cliente/u04/U04-A04-Cristina/U04-A04-Cristina.js
@@ -0,0 +1,50 @@
+var respuesta = document.getElementById('respuesta');
+var boton = document.getElementById('boton');
+var impares = [];
+
+function creaImpares(impares) {
+    for (let i = 1; i <= 21; i++) {
+        if (( i % 2 ) != 0 ) {
+            impares.push(i);
+        }
+    }
+    return impares;
+}
+
+function sumaImpares(impares) {
+    return impares.slice().reduce((a, b) => a + b, 0);
+}
+
+function buscaValor(){
+    let valor = document.getElementById('valor').value;
+    let index = impares.indexOf(Number(valor));
+    if ( index != -1 ) {
+        escribir(`Se ha encontrado el valor ${valor} y se ha borrado`);
+        impares.splice(index, 1);
+        escribir('Nuevo array ' + impares.join(', '));
+    } else {
+        escribir(`No se ha encontrado el valor ${valor}.`)
+    }
+    escribir('Ascendente: ' + ordenaAscendente(impares));
+    escribir('Descendente: ' + ordenaDescendente(impares));
+}
+
+function ordenaAscendente(impares) {
+    return impares.sort(function (a, b) { a - b });
+}
+
+function ordenaDescendente(impares) {
+
+    return impares.slice().sort(function (a, b) { b - a });
+}
+
+function escribir(res) {
+    let p = document.createElement('p');
+    p.textContent = res;
+    respuesta.append(p);
+}
+
+escribir('Array de impares: ' + creaImpares(impares));
+escribir('Suma: ' + sumaImpares(impares));
+escribir('Inverso: ' + impares.slice().reverse());
+boton.addEventListener('click', buscaValor);
