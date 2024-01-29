ceb70196762eb186cfa290c0ee1b574347959ff9
Cristina Rivera <cristinapruebas14@gmail.com>
2024-01-29 19:30:03 +0100

    .

--- cliente/u05/práctica examen/practica.js
+++ cliente/u05/práctica examen/practica.js
@@ -1,23 +1,32 @@
 const imagen = document.getElementById('imagen');
 
 imagen.addEventListener('mousedown', cambiarImagen);
-imagen.addEventListener('mouseup', volverImagen);
+document.addEventListener('mouseup', volverImagen);
 
+
+imagen.ondragstart = function() {
+    return false;
+}
 function cambiarImagen() {
     imagen.src = 'gato(3).png';
+    imagen.addEventListener('mousemove', moverImagen);
 }
 
-function volverImagen(e) {
+function volverImagen() {
+    imagen.removeEventListener('mousemove', moverImagen);
     imagen.src = 'gato.png';
 }
 
-imagen.addEventListener('dragend', moverImagen);
 
 function moverImagen(e) {
-    imagen.src = 'gato.png';
     imagen.style.top = e.clientY - (imagen.clientHeight / 2) + 'px';
     imagen.style.left = e.clientX - (imagen.clientWidth / 2) + 'px';
 }
+
+
+
+
+
 
 const cambiar = document.getElementById('cambiar');
 const input = document.getElementById('img');
