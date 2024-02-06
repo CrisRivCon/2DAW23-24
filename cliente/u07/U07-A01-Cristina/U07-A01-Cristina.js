f8072a062a2254682b639f5e30189b2ced92c873
Cristina Rivera <cristinapruebas14@gmail.com>
2024-02-06 21:15:47 +0100

    .

--- cliente/u07/U07-A01-Cristina/U07-A01-Cristina.js
+++ cliente/u07/U07-A01-Cristina/U07-A01-Cristina.js
@@ -1,11 +1,29 @@
 const contenedor = document.getElementById('contenedor');
+const url = "U07-A01-Cristina.json";
 
-fetch("U07-A01-Cristina.json")
-.then(response=> response.json())
+/* fetch(url)
+.then(response => response.json())
 .then(datos => mostrarDatos(datos))
+*/
+trabajadores(url);
 
+async function trabajadores(requestUrl) {
+    const request = new Request(requestUrl);
+    
+    const response = await fetch(request);
+    const texto = await response.text();
+
+    const empleados = JSON.parse(texto);
+    mostrarDatos(empleados);
+}
 
 function mostrarDatos(valor) {
-    let p = document.createElement('p');
-    
+    console.log(valor);
+    let trabajadores = valor.departamento.trabajador;
+
+    trabajadores.forEach(trabajador => {
+        let p = document.createElement('p');
+        p.textContent = trabajador.nombre + ' ' + trabajador.apellidos;
+        contenedor.append(p);
+    });
 }
