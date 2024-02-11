2e5550a16f2b6895f28ec08d5b076adab1e6cffd
Cristina Rivera <cristinapruebas14@gmail.com>
2024-02-11 14:29:52 +0100

    .

--- cliente/u07/U07-A03-Cristina/U07-A03-Cristina.js
+++ cliente/u07/U07-A03-Cristina/U07-A03-Cristina.js
@@ -1,18 +1,24 @@
 const inputs = document.getElementsByName('equipo');
 const url = 'encuesta_voto.php';
 
+const progress = document.querySelectorAll('.barra');
+
+
 inputs.forEach(input => {
     input.addEventListener('input', actualizarPuntuacion);
 });
 
 function actualizarPuntuacion(e) {
-    console.log('hola');
     let nombre = e.target.id;
 
     fetch(`${url}?nombre=${nombre}`)
-    .then(response=>response.text())
+    .then(response=>response.json())
     .then(data => {
-        console.log(data);
+        let porcentajes = data.porcentajes;
+        progress.forEach((barra, i) => {
+            barra.value = porcentajes[i];
+            barra.max = data.total;
+        })
     })
 
 }
--- cliente/u07/U07-A03-Cristina/encuesta_voto.php
+++ cliente/u07/U07-A03-Cristina/encuesta_voto.php
@@ -1,8 +1,32 @@
 <?php
-
+// Parametros constantes
 $url = 'resultado.txt';
-$nombre = $_GET['nombre'];
 $nombres = ['real', 'barcelona', 'atletico', 'sevilla'];
+$nombre = $_GET['nombre'];
 
+// Extraer el contenido y convertirlo en array
 $contenido = file_get_contents($url);
-echo $nombre;
+$puntuaciones = explode('||', $contenido);
+
+// Conseguir la posición a cambiar del array de puntuacion y aumentar la puntuación
+$posicion = array_search($nombre, $nombres);
+$puntuaciones[$posicion]++;
+
+// Actualizar la puntuación en el archivo txt
+file_put_contents($url, implode('||', $puntuaciones));
+
+// Calcular los porcentajes
+$votos_totales = array_sum($puntuaciones);
+
+function porcentaje($valor)
+{
+    global $votos_totales;
+    return round($valor * 100 / $votos_totales);
+}
+
+$porcentajes = array_map("porcentaje", $puntuaciones);
+
+// Devolver el array con las puntuciones y los prcentajes
+$respuesta = ["puntuaciones" => $puntuaciones, "porcentajes" => $porcentajes, "total" => $votos_totales];
+
+echo json_encode($respuesta);
